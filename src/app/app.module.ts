abbbcf58c1e9a80db4c1e0ad707ea0ca1b124789
donkeykong91 <darylnator@gmail.com>
2020-04-15 05:53:06 -0700

    new files and updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,11 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-import { ContactListComponent } from './contact-list/contact-list.component';
-import { ContactSingleComponent } from './contact-single/contact-single.component';
-import { CreateContactComponent } from './create-contact/create-contact.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ContactListComponent,
-    ContactSingleComponent,
-    CreateContactComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
